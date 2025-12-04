dede902b4280500ce7d68e71f1db9331f1559cce
flinxx1026-cpu <flinxx1026@gmail.com>
2025-12-04 22:11:59 +0530

    Add Prisma migrations: init schema and admin user insert

---
+++ backend/prisma/migrations/1_insert_admin/migration.sql
@@ -0,0 +1,3 @@
+-- Insert admin user
+INSERT INTO "Admin" (email, password, role, "createdAt", "updatedAt")
+VALUES ('Nikhilyadav1026@flinxx.com', '$2a$10$hw//L5nGXC7fMrTjFOWnHOZJ5XTPJ9PhabzGX4GqLwYClj0haZFae', 'ADMIN', NOW(), NOW());
